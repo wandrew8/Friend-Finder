ff31040e35d2537f69809ae45431377ce1a1134b
Andrew Weiss <wandrew8@gmail.com>
2019-10-03 22:47:45 -0700

    Front end HTML added and server connected

---
+++ app/data/friends.js
@@ -0,0 +1,51 @@
+var friendsArray = [
+    {
+        name: "Ralph Finnigan",
+        scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        summary: "Howdy, buddy. It looks like we're a match! My name is Ralph Finnigan. I'm sure we can become good friends. Send me a message. I'd love to learn more about you.",
+        photo: "https://cdn.pixabay.com/photo/2015/08/05/04/02/people-875597_1280.jpg"
+    },
+    {
+        name: "Sara Summers",
+        scores: [1, 2, 1, 2, 1, 2, 1, 2, 1, 2],
+        summary: "Heya! My name is Sara Summers. I'm so glad that we're a match. I'm sure we can become good friends. Send me a message. I'd love to learn more about you.",
+        photo: "https://cdn.pixabay.com/photo/2016/03/27/21/15/party-1284304_1280.jpg"
+    },
+    {
+        name: "Cody Mathews",
+        scores: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+        summary: "Hey, how's it going? My name is Cody. I'm so glad that we're a match. I'm sure we can become good friends. Send me a message. I'd love to learn more about you.",
+        photo: "https://cdn.pixabay.com/photo/2018/04/27/03/50/portrait-3353699_1280.jpg"
+    },
+    {
+        name: "Josh Andrews",
+        scores: [2, 3, 2, 3, 2, 3, 2, 3, 2, 3],
+        summary: "Hello! My name is Josh Andrews. I'm so glad that we're a match. I'm sure we can become good friends. Send me a message. I'd love to learn more about you.",
+        photo: "https://cdn.pixabay.com/photo/2016/09/24/03/20/passion-1690965_1280.jpg"
+    },
+    {
+        name: "Leah Micheals",
+        scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+        summary: "Hi! My name is Leah. I'm so glad that we're a match. I'm sure we can become good friends. Send me a message. I'd love to learn more about you.",
+        photo: "https://cdn.pixabay.com/photo/2017/02/16/23/10/smile-2072907_1280.jpg"
+    },
+    {
+        name: "John Andrews",
+        scores: [3, 4, 3, 4, 3, 4, 3, 4, 3, 4],
+        summary: "Hey, My name is John. I'm so glad that we're a match. I'm sure we can become good friends. Send me a message. I'd love to learn more about you.",
+        photo: "https://cdn.pixabay.com/photo/2015/07/20/07/12/people-852428_1280.jpg"
+    },
+    {
+        name: "Andrew Johnsom",
+        scores: [4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
+        summary: "How's it going? My name is Andrew Johnson. I'm so glad that we're a match. I'm sure we can become good friends. Send me a message. I'd love to learn more about you.",
+        photo: "https://cdn.pixabay.com/photo/2017/08/12/18/31/male-2634974_1280.jpg"
+    },
+    {
+        name: "Eric Laurence",
+        scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
+        summary: "Hey, Eric here. I'm so glad that we're a match. I'm sure we can become good friends. Send me a message. I'd love to learn more about you.",
+        photo: "https://cdn.pixabay.com/photo/2015/08/14/15/28/smiling-888532_1280.jpg"
+    }
+
+];
